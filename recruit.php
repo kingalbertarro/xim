0a7b686c94a85441a971efb297a3fc625ac4ae32
King Albert C. Arro - Marilou <kingalbertarro@gmail.com>
2018-01-11 14:39:58 +0800

    Updates

---
+++ recruit.php
@@ -0,0 +1,142 @@
+<?php include 'header.php'; ?>
+
+ <div id="p-banner"> <!-- #p-banner -->
+	<section class="other-banner">
+    <img src="images/common/logo.png" alt="">
+    <h2>RECRUIT</h2>
+    <p>採用情報</p>
+  </section>
+ </div> <!--// #p-banner -->
+
+<div id="p-contents-wrap"> <!-- #p-contents-wrap -->
+  <div id="p-contents"> <!-- #p-contents -->
+
+    <h2 class="main-title">募集要項<span class="trans">Requirements</span></h2>
+    <section class="requirements">
+      <aside class="container">
+        <table class="requirements-lists">
+          <tr>
+            <td>職種</td>
+            <td>営業 / マーケティング / 事業統括マネジャー / Webデザイナー兼エンジニア / 総務</td>
+          </tr>
+          <tr>
+            <td>雇用形態</td>
+            <td>正社員 / 契約社員 / アルバイト</td>
+          </tr>
+        </table>
+      </aside>
+    </section>
+
+
+    <h2 class="main-title">企業情報<span class="trans">Company Informations</span></h2>
+    <section class="company-information">
+      <aside class="container">
+        <div class="gallery">
+          <div class="slider slider-single">
+            <div><figure class="cover"><img src="images/common/sample01.jpg" alt=""></figure></div>
+            <div><figure class="cover"><img src="images/common/sample01.jpg" alt=""></figure></div>
+            <div><figure class="cover"><img src="images/common/sample01.jpg" alt=""></figure></div>
+            <div><figure class="cover"><img src="images/common/sample01.jpg" alt=""></figure></div>
+			    </div>
+			    <div class="slider slider-nav">
+            <div class="slider-items"><figure class="cover"><img src="images/common/sample01.jpg" alt=""></figure></div>
+            <div class="slider-items"><figure class="cover"><img src="images/common/sample01.jpg" alt=""></figure></div>
+            <div class="slider-items"><figure class="cover"><img src="images/common/sample01.jpg" alt=""></figure></div>
+            <div class="slider-items"><figure class="cover"><img src="images/common/sample01.jpg" alt=""></figure></div>
+			    </div>
+        </div>
+        <article class="detailed-info">
+          <table class="info-lists">
+            <tr>
+              <td>商号</td>
+              <td>株式会社XiM</td>
+            </tr>
+            <tr>
+              <td>本社所在地</td>
+              <td>
+                〒151-005<br />
+                東京都渋谷区千駄ヶ谷四丁目16番7号北参道DTビル5階<br>
+                <a href="">拠点一覧はこちら≫</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                設立
+              </td>
+              <td>
+                2017年4月19日
+              </td>
+            </tr>
+            <tr>
+              <td>
+                資本金
+              </td>
+              <td>
+                1,000万円
+              </td>
+            </tr>
+            <tr>
+              <td>
+                代表取締役
+              </td>
+              <td>
+                雨瀧　浩一郎
+              </td>
+            </tr>
+            <tr>
+              <td>
+                取締役
+              </td>
+              <td>
+                本田　真也
+              </td>
+            </tr>
+            <tr>
+              <td>
+                従業員数
+              </td>
+              <td>
+                40名（社員31名・アルバイト9名）
+              </td>
+            </tr>
+            <tr>
+              <td>
+                事業内容
+              </td>
+              <td>
+                IT通信事業 <br/>
+                HR事業 <br/>
+                Google事業 <br/>
+                パートナー事業 <br/>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                取引先金融機関
+              </td>
+              <td>
+                三菱東京UFJ銀行<br>
+                三井住友銀行<br>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                お問い合わせ
+              </td>
+              <td>
+                TEL 03-4405-5577<br>
+                FAX 03-4243-3300<br>
+                E-mail info@xim.co.jp<br>
+                <a href="">お問い合わせフォーム≫</a><br>
+                <a href="">オンラインルーム≫</a>
+              </td>
+            </tr>
+          </table>
+        </article>
+      </aside>
+    </section>
+
+	</div> <!-- // #p-contents -->
+ </div> <!-- // #p-contents-wrap -->
+
+ <?php include 'footer.php'; ?>
